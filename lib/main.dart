de78318c5e4a49fe414e4a86915120fe665d8c1f
Samuel Sousa <samuel.sousa.viana@live.com>
2023-03-15 16:28:32 -0300

    Implementatio of a theme to the AppBars

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:photo_gallery/app/injector/binds_injector.dart';
 import 'package:photo_gallery/app/routes/app_navigator.dart';
 import 'package:photo_gallery/app/routes/app_router.dart';
@@ -16,8 +17,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Photo Gallery',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      navigatorKey: GetIt.I.get<AppNaviagator>().navigatorKey,
+      theme: ThemeData(
+          fontFamily: GoogleFonts.inter().fontFamily,
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white,
+            elevation: 0,
+            iconTheme: const IconThemeData(color: Colors.black),
+            centerTitle: true,
+            titleTextStyle: GoogleFonts.inter(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          )),
+      navigatorKey: GetIt.I.get<AppNavigator>().navigatorKey,
       initialRoute: AppRouter.initialRoute,
       onGenerateRoute: AppRouter.onGenerateRoute,
     );
