972f7284324f451d987004a5ef5e72dd104e3b76
Samuel Sousa <samuel.sousa.viana@live.com>
2023-03-15 00:44:50 -0300

    Implementation of BindsInjector

---
+++ lib/app/injector/binds_injector.dart
@@ -0,0 +1,45 @@
+import 'package:get_it/get_it.dart';
+import 'package:photo_gallery/app/injector/injector.dart';
+
+class BindsInjector extends Injector {
+  BindsInjector(super.getItInstance);
+
+  @override
+  void controllers(GetIt i) {
+    // TODO: implement controllers
+  }
+
+  @override
+  void core(GetIt i) {
+    // TODO: implement core
+  }
+
+  @override
+  void datasources(GetIt i) {
+    // TODO: implement datasources
+  }
+
+  @override
+  // TODO: implement getItInstance
+  GetIt get getItInstance => throw UnimplementedError();
+
+  @override
+  void registerAll() {
+    // TODO: implement registerAll
+  }
+
+  @override
+  void repositories(GetIt i) {
+    // TODO: implement repositories
+  }
+
+  @override
+  void stores(GetIt i) {
+    // TODO: implement stores
+  }
+
+  @override
+  void useCases(GetIt i) {
+    // TODO: implement useCases
+  }
+}
