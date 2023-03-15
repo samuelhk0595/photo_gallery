7358a00bd6fa8b3b3717ec6563e65c19dfe000b4
Samuel Sousa <samuel.sousa.viana@live.com>
2023-03-15 00:17:28 -0300

    Fix name of PhotoRepository

--- lib/app/domain/photo/repositories/photo_repository.dart
+++ lib/app/domain/photo/repositories/photo_repository.dart
@@ -2,6 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:photo_gallery/app/core/failure.dart';
 import 'package:photo_gallery/app/domain/photo/entities/photo_entity.dart';
 
-abstract class JsonPlaceholderRepository {
+abstract class PhotoRepository {
   Future<Either<Failure, List<PhotoEntity>>> getPhotos();
 }
